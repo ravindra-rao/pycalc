4a2e99efb971a634352c791542eb4f509dbfdf39
ravindra.rao <ravindra.r1992@gmail.com>
2023-03-13 08:41:29 -0300

    updated release version 0.0.2.7 and realigned filter in yml

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     include_package_data=True, 
     name='yopycalc', 
-    version='0.0.2.6', 
+    version='0.0.2.7', 
     description='yopycalc python module',
     url='https://github.com/ravindra-rao/setuptoolsdemo.git',
     author='yobro',
