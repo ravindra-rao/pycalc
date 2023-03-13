7490934c2ea836fb69d7f0a5317d6097273a6169
ravindra.rao <ravindra.r1992@gmail.com>
2023-03-13 09:12:35 -0300

    updated release version 0.0.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     include_package_data=True, 
     name='yopycalc', 
-    version='0.0.2.10', 
+    version='0.0.3.1', 
     description='yopycalc python module',
     url='https://github.com/ravindra-rao/setuptoolsdemo.git',
     author='yobro',
