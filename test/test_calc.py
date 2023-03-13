181f8a46f1b7399f43d54eb1e0bf13b99cfe38df
ravindra.rao <ravindra.r1992@gmail.com>
2023-03-13 02:58:26 -0300

    updated testcase to use unittest

--- test/test_calc.py
+++ test/test_calc.py
@@ -3,19 +3,33 @@ Created on Mar. 12, 2023
 
 @author: wearo
 '''
+import unittest
+from yopycalc.calc import Calc
 
-from yopycalc.calc import add
-
-import sys
-for i in sys.path:
-    print(i)
+# import sys
+# for i in sys.path:
+#     print(i)
     
 
+class TestCalc(unittest.TestCase):        
+    def setUp(self):
+        self.calc = Calc(2, 3)
 
-def test_add():
-    assert add(2,3) ==5
-    print("add Function works correctly")
+    def test_add_positive(self):
+        # self.assertEqual(self.calc.add(), 5, "msg add Function is not working correctly")
+        # self.assertEqual(self.calc.add(), 6, f"{__file__}: msg add Function is not working correctly")
+        self.assertEqual(self.calc.add(), 5, f"{__file__}: msg add Function is not working correctly")
 
-test_add()
-# if __name__ == '__main__':
-#     test_add()
+        # print("add Function works correctly")
+
+def suite():
+    """
+        Gather all the tests from this module in a test suite.
+    """
+    suite = unittest.TestSuite()
+    suite.addTest(TestCalc('test_add_positive')) 
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())
