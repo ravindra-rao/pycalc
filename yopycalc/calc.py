3c31bb6879809983fdcdc397104de1a585156ac1
ravindra.rao <ravindra.r1992@gmail.com>
2023-03-16 04:31:28 -0300

    added calc.py

---
+++ yopycalc/calc.py
@@ -0,0 +1,27 @@
+'''
+Created on Mar. 16, 2023
+
+@author: wearo
+'''
+class Calc:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a + self.b
+    
+    def sub(self):
+        return self.a - self.b
+    
+    def mul(self):
+        return self.a * self.b
+    
+    def div(self):
+        return self.a / self.b
+
+def SayHello():
+    print("sup world from srcmake")
+    
+if __name__ == '__main__':
+    SayHello()
+
