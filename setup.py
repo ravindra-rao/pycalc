b60d2852af6ebab00518a0c85b8587a173f4b380
Ravindra Rao <ravindra.r1992@gmail.com>
2023-03-16 05:12:15 -0300

    Develop (#10)
    * renamed old files
    
    * added calc.py
    
    * added setup.py
    
    * added circleci config file
    
    * added tests
    
    updated gitignore and version in setup.py
    
    added build files generated by wheel to gitignore

---
+++ setup.py
@@ -0,0 +1,26 @@
+'''
+Created on Mar. 16, 2023
+
+@author: wearo
+'''
+import setuptools
+
+#Adding code for packaging
+setuptools.setup(
+    include_package_data=True, 
+    name='yopycalc', 
+    version='0.0.3.5', 
+    description='yopycalc python module',
+    url='https://github.com/ravindra-rao/setuptoolsdemo.git',
+    author='yobro',
+    author_email='contact@yobro.com',
+    packages=setuptools.find_packages(),
+    install_requires=['pandas', 'pytest'
+    ], 
+    long_description='yopycalc python module',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+)
