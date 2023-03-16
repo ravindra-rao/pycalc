4995313f02aa0b184a92dbdc805259e59ac39565
ravindra.rao <ravindra.r1992@gmail.com>
2023-03-16 07:36:17 -0300

    added README and updated release version

---
+++ README.md
@@ -0,0 +1,8 @@
+# yopycalc
+Python Calculator
+
+Simple python calculator
+
+Link to PyPI: https://pypi.org/project/yopycalc/
+
+Install package on your local machine using the command: pip install yopycalc
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import setuptools
 setuptools.setup(
     include_package_data=True, 
     name='yopycalc', 
-    version='0.0.3.5', 
+    version='0.0.3.7', 
     description='yopycalc python module',
     url='https://github.com/ravindra-rao/setuptoolsdemo.git',
     author='yobro',
